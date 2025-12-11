94f9034830351dcb6d90c4995b0c4079ec1cb347
AlirezaTashani <alirezatashanipr@gmail.com>
2025-12-11 20:07:47 +0330

    basic tel code app

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import telebot
 import os
 from dotenv import load_dotenv
-from telebot.types import Message
+from telebot.types import Message , ReplyKeyboardMarkup , InlineKeyboardButton , InlineKeyboardMarkup
+from text import help_unit , help_base
+
 
 #load token and use
 load_dotenv()
@@ -10,11 +12,64 @@ TOKEN = os.getenv('API_TOKEN')
 #CREATE BOT API
 bot = telebot.TeleBot(token=TOKEN)
 
-@bot.message_handler('start')
+#button for app
+key_markup = ReplyKeyboardMarkup(resize_keyboard=True,row_width=2 )
+key_markup.add('Unit' , 'Base' , 'Date' , 'About')
+
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback(call):
+    if call.data == 'unit_btn':
+        bot.send_message(call.message.chat.id, f'{help_unit}' )
+    elif call.data == 'base_btn':
+        bot.send_message(call.message.chat.id , f'{help_base}') 
+
+
+@bot.message_handler(commands='start')
 def say_hi(message : Message):
-    bot.reply_to(message , 'Hi 👋 welcome to Transmuter ✨ \nHere you can perform any conversion you want 🔄')
+    bot.reply_to(message , 'Hi 👋 Welcome to Transmuter ✨\nHere you can perform any conversion you like 🔄' , reply_markup = key_markup)
     
-    
+
+@bot.message_handler()
+def convering(message:Message):
+    if message.text == 'Unit':
+        help_for_unit = InlineKeyboardButton('help' , callback_data='unit_btn')
+        markup_unit = InlineKeyboardMarkup().add(help_for_unit)
+        bot.send_message(message.chat.id , f'📏 Enter a value with its unit, like 20 km',reply_markup=markup_unit)
+        
+        
+        
+    elif message.text == "Base":
+        help_for_base = InlineKeyboardButton('help' , callback_data='base_btn')
+        markup_base = InlineKeyboardMarkup().add(help_for_base)
+        bot.send_message(message.chat.id , f'🔢 Enter a number with its base, like 1010 b or 1F h',reply_markup=markup_base)
+        
+
+    elif message.text == 'Date' :
+        date_btn = ReplyKeyboardMarkup(resize_keyboard=True)
+        date_btn.add('Gregorian', 'Persian' , 'Islamic' , 'back')
+        bot.send_message(message.chat.id , f'📅 Choose the input calendar' , reply_markup=date_btn)
+
+    elif message.text == 'About' :
+        bot.send_message(message.chat.id , f"""👋 Hi, I'm Alireza Tashani  
+💻 Python Developer & Programmer  
+📧 Email: alirezatd80@gmail.com
+""")
+    elif message.text == 'Gregorian':
+        bot.send_message(message.chat.id , 'Enter Date Gregorian📅 :') 
+    elif message.text == 'Persian':
+        bot.send_message(message.chat.id , 'Enter Date Persian🌞 :') 
+    elif message.text == 'Islamic':
+        bot.send_message(message.chat.id , 'Enter Date Islamic🌙 :') 
+    elif message.text == 'back':
+        bot.send_message(message.chat.id , '📍 Main Menu' , reply_markup = key_markup)
+    else:
+        bot.send_message(message.chat.id , f'⚠️ Invalid input type.')
+
+
+
+        
+      
 bot.polling(
     skip_pending=True,
 )
---
+++ text.py
@@ -0,0 +1,58 @@
+help_unit = """
+🌡️ Temperature Units:
+  • Celsius (°C)
+  • Fahrenheit (°F)
+  • Kelvin (K)
+
+📏 Length Units:
+  • Meter (m)
+  • Kilometer (km)
+  • Centimeter (cm)
+  • Millimeter (mm)
+  • Mile (mi)
+  • Yard (yd)
+  • Foot (ft)
+  • Inch (in)
+
+⚖️ Weight / Mass Units:
+  • Gram (g)
+  • Kilogram (kg)
+  • Milligram (mg)
+  • Pound (lb)
+  • Ounce (oz)
+
+
+🧪 Volume Units:
+  • Liter (L)
+  • Milliliter (mL)
+  • Cubic Meter (m3)
+  • Gallon (gal)
+
+💨 Speed Units:
+  • Meter per Second (m/s)
+  • Kilometer per Hour (km/h)
+  • Mile per Hour (mph)
+
+⏱️ Time Units:
+  • Second (s)
+  • Minute (min)
+  • Hour (h)
+  • Day (d)
+
+🧠 Data Units:
+  • Bit (b)
+  • Byte (B)
+  • Kilobyte (KB)
+  • Megabyte (MB)
+  • Gigabyte (GB)
+  • Terabyte (TB)
+
+"""
+
+help_base = """
+🔢 Base Number Systems:
+  • Binary (base 2) — 0b -> 1011 b
+  • Octal (base 8) — 0o -> 14 o
+  • Decimal (base 10) — 0d -> 12 d
+  • Hexadecimal (base 16) — 0x -> 2F x
+"""
